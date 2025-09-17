9d1882e006bc42e60c9fd2601a1b911912a88361
tgede46 <gedeonkpara@gmail.com>
2025-09-17 01:22:04 +0000

    Initialize Spring Boot project with Maven wrapper, database configuration, and basic application structure

---
+++ avis-utilisateur/src/main/java/gedeon/net/avis/AvisUtilisateurApplication.java
@@ -0,0 +1,13 @@
+package gedeon.net.avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisUtilisateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisUtilisateurApplication.class, args);
+	}
+
+}
---
+++ avis-utilisateur/src/main/java/gedeon/net/avis/entite/Avis.java
@@ -0,0 +1,9 @@
+package gedeon.net.avis.entite;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "avis")
+public class Avis {
+}
